4c319739fc3767050792cd006147c4fa46d39e65
han <284807225@qq.com>
2022-01-30 13:21:33 +0800

    276bobbyHw1

---
+++ assignment1.js
@@ -0,0 +1,12 @@
+function addLine(){
+    var tableIF=document.getElementById("mytable");
+    var tr=document.createElement("tr");
+    var td1=document.createElement("td");
+    td1.innerHTML="hhhh";
+    tr.appendChild(td1);
+    tableIF.appendChild(tr);
+}
+function show(){
+    var w=document.getElementById("w1");
+    
+}
